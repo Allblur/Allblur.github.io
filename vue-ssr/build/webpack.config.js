3e02fed0a450986ef6d11259af867637b25a6ad0
liaojj <jijun0117>
2019-02-22 14:35:59 +0800

    update

--- vue-ssr/build/webpack.config.js
+++ vue-ssr/build/webpack.config.js
@@ -37,6 +37,52 @@ const getEntry = function(pathArr) {
 
 const entries = getEntry(['./app/pages/*.js', './app/pages/**/*.js'])
 
+/*
+
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
+module.exports = {
+  optimization: {
+    minimizer: [
+      // 自定义js优化配置，将会覆盖默认配置
+      new UglifyJsPlugin({
+        exclude: /\.min\.js$/, // 过滤掉以".min.js"结尾的文件，我们认为这个后缀本身就是已经压缩好的代码，没必要进行二次压缩
+        cache: true,
+        parallel: true, // 开启并行压缩，充分利用cpu
+        sourceMap: false,
+        extractComments: false, // 移除注释
+        uglifyOptions: {
+          compress: {
+            unused: true,
+            warnings: false,
+            drop_console: true,
+            drop_debugger: true
+          },
+          output: {
+            comments: false
+          }
+        }
+      }),
+      // 用于优化css文件
+      new OptimizeCssAssetsPlugin({
+        assetNameRegExp: /\.css$/g,
+        cssProcessorOptions: {
+          safe: true,
+          autoprefixer: { disable: true }, // 这里是个大坑，稍后会提到
+          mergeLonghand: false,
+          discardComments: {
+            removeAll: true // 移除注释
+          }
+        },
+        canPrint: true
+      })
+    ]
+  }
+}
+
+ */
+
 const webpackConfig = {
 	mode: isDev ? 'development' : 'production',
 	target: 'web',
