5bcb6dfa9a240841f527055959dc740c1c80fefa
douliop <douliop@gmail.com>
2019-03-11 22:37:25 +0800

    add file

--- vue-ssr/app/views/Home.vue
+++ vue-ssr/app/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="app">
 		<h1>this is home page</h1>
+		<input type="checkbox" v-model="checkboxData" :value="checkbox1">
+		<input type="checkbox" v-model="checkboxData" :value="checkbox2">
 	</div>
 </template>
 <script>
@@ -8,8 +10,17 @@
 		name: 'homePage',
 		data() {
 			return {
-				
+				checkboxData: [[1,2,3], [4,5,6]],
+				checkbox1: [1,2,3],
+				checkbox2: [3,4,5]
 			}
+		},
+		mounted() {
+			this.$nextTick(() => {
+				setTimeout(() => {
+					this.checkbox2 = [4,5,6]
+				}, 10*1000);
+			})
 		}
 	}
 </script>
